c4d995dbc62a97d3b346da69ed7a0454c99f2e13
Mentor <mentor@palokaj.co>
2022-03-17 11:23:36 +0100

    💚 play nice with eslint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -9,6 +9,10 @@ module.exports = {
         "ecmaVersion": 12
     },
     rules: {
+        "no-case-declarations": 0,
+        "prefer-arrow-callback": 2,
+        "no-mixed-spaces-and-tabs": 1,
+        "no-unused-vars": [ 1, { vars: 'all', args: 'none' } ], // All variables, no function arguments
         "no-control-regex": 0
     }
 }
